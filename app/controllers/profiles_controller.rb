71aef6be1e5db522ea33f22e86450389916beee1
Omer Siddiqui <oasiddiqui1@gmail.com>
2026-03-07 00:09:27 -0600

    feat: add profile routes and controller

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,27 @@
+class ProfilesController < ApplicationController
+  before_action :authenticate_user!, only: [:edit, :update]
+
+  def show
+    @user = User.find_by!(username: params[:username])
+  end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+
+    if @user.update(profile_params)
+      redirect_to user_profile_path(@user.username), notice: "Profile updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def profile_params
+    params.require(:user).permit(:username, :display_name, :bio)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
 
   root "home#index"
 
+  resource :profile, only: [:edit, :update]
+  get "/u/:username", to: "profiles#show", as: :user_profile
+
   get "up" => "rails/health#show", as: :rails_health_check
 end

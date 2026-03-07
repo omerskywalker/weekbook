499380af52a102fdc0fb77d72933cffd79cec290
Omer Siddiqui <oasiddiqui1@gmail.com>
2026-03-07 00:06:13 -0600

    feat: add profile fields to users

---
+++ db/migrate/20260307060526_add_profile_fields_to_users.rb
@@ -0,0 +1,9 @@
+class AddProfileFieldsToUsers < ActiveRecord::Migration[7.1]
+  def change
+    add_column :users, :username, :string
+    add_column :users, :display_name, :string
+    add_column :users, :bio, :text
+
+    add_index :users, :username, unique: true
+  end
+end

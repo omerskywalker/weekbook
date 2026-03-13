4dc07cc5e1592d4c12f96c472d6db2bcad49c5f5
Omer Siddiqui <oasiddiqui1@gmail.com>
2026-03-13 00:01:25 -0500

    formatted code + tested code

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,21 +21,21 @@ class User < ApplicationRecord
   end
 
   has_many :active_follows,
-         class_name: 'Follow',
-         foreign_key: :follower_id,
-         dependent: :destroy,
-         inverse_of: :follower
+           class_name: 'Follow',
+           foreign_key: :follower_id,
+           dependent: :destroy,
+           inverse_of: :follower
 
   has_many :passive_follows,
-          class_name: 'Follow',
-          foreign_key: :followed_id,
-          dependent: :destroy,
-          inverse_of: :followed
+           class_name: 'Follow',
+           foreign_key: :followed_id,
+           dependent: :destroy,
+           inverse_of: :followed
 
   has_many :following, through: :active_follows, source: :followed
   has_many :followers, through: :passive_follows, source: :follower
 
   def following?(other_user)
-  following.exists?(other_user.id)
+    following.exists?(other_user.id)
   end
 end
